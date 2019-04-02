0386a422eb243ba3a8a33e3b5155a658377891ad
ihor5433 <iihor5433@gmail.com>
2019-04-02 11:46:17 -0400

    Ne_Dodelal

--- Learn_JS/new.js
+++ Learn_JS/new.js
@@ -1,1 +1,14 @@
-new.js
+function result() {
+    var a, b, p;
+    c = document.getElementById('out');
+    a = document.getElementById('num1').value;
+    a = parsentInt(a);
+    b = document.getElementById('num2').value;
+    b = parseInt(b);
+    if (a > b) {
+        p.innerHTML = 'Первое число больше второго';
+
+    } else if (a < b) {
+
+        p.innerHTML = 'Первое число меньше второго';
+    }
