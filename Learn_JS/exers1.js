030017a6ca3b097f1cca765960928bccdf014850
ihor5433 <iihor5433@gmail.com>
2019-04-03 23:02:21 -0700

    1

--- Learn_JS/exers1.js
+++ Learn_JS/exers1.js
@@ -1,26 +1,15 @@
-var location1 = 3;
-var location1 = 4;
-var location1 = 5;
-var guess; // координаты выстрела
-var hits = 0;
-var guesses = 0;
-var isSunk = false; // станет true когда кораблю потонет
-while (isSunk == false); {
-guess = prompt("Введите точку выстрела (1-6)");
-if (guess < 0 || guess > 6  ) {
-	alert ('Выберите правильный номер');
-} else {
-	guesses = guesses + 1;
-	if (guess == location1 || guess == location2 || guess == location3) {
-	hits = hits + 1;
-
-		if (hits == 3) {
-			isSunk = true;
-	alert("Ты потопил корабль")
-			 }
-		}
+function f1() {
+	var p = document.getElementById('out');
+	var p1 = document.getElementById('out1');
+	var i = 0;
+	while (i < 50) {
+		p.innerHTML += i + ' ';
+		i++
 	}
+	var j = 30;
+	do {
+		p1.innerHTML += j + ' ';
+		j--;
+	}
+	while (j < 30);
 }
-var stats = "You took " + guesses + " guesses to sink the battleship, " +
-				"which means your shooting accuracy was " + (3/guesses);
-alert(stats);
