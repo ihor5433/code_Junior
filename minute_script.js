a86d045a3888d86375a06ff0576c91a1ed850a58
ihor5433 <iihor5433@gmail.com>
2019-04-04 11:34:48 -0400

    1

--- minute_script.js
+++ minute_script.js
@@ -1,4 +1,10 @@
-var second = 60;
-var minutes= 60;
-var secondsInHours = second*minutes;
-console.log ('В часе '+secondsInHours+' секунд')
+function f1() {
+	var a=document.getElementById('stach').value;
+	a = parseInt(a);
+	var p=document.getElementById("out")
+	percent= (a>=0 && a<=3) ? "Надбавка 0%" :
+			 (a>=4 && a<=10) ? "Надбавка 10%" :
+			( a>=11 && a<=20) ? "Надбавка 20%" :
+			 (a>=21) ? "Надбавка 25%" : "Неправильный стаж"
+	p.innerHTML = (percent);
+} 
